fe87f6204ccdd91f966761204f6aefe1e394d475
Guillermo Valentín <masweb@me.com>
2024-02-14 23:05:49 +0100

    copy curr

---
+++ components/Dark.client.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import { useDark, useToggle } from '@vueuse/core'
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+</script>
+
+<template>
+    <button class="dark" @click="toggleDark()">
+      <i inline-block align-middle i="dark:carbon-moon carbon-sun" />
+      <span class="ml-2">{{ isDark ? 'Dark' : 'Light' }}</span>
+    </button>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,229 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <Dark/>
+
+    <div class="page">
+      <div class="clearfix">
+        <div class="sm-col sm-col-6 mb3  person">
+          <img class="rounded" src="../public/pic.png" height="335">
+          <div class="signs">
+            <div class="name mt2">Guillermo Valentín Sánchez</div>
+            <div class="job mt1  ">Desarrollador front end. Diseñador gráfico.</div>
+            <p class="job ">Santa Pola  / Alicante • Edad: 14/02/73</p>
+          </div>
+        </div>
+        <div class="sm-col sm-col-6  ">
+          <p class="text">Desde el año 1992 he trabajado en entornos de edición y publicidad.
+            Entusiasta de las tecnologías y desarrollo de software, autodidacta, codificando profesionalemente desde 2012.</p>
+
+          <p class="text">Desarrollo de grandes proyectos, apps nocode, experto en Vue, Typescript y nuxt.</p>
+          <p class="text">Usabilidad, interfaces gráficas, WYSIWYG, multiidioma.</p>
+          <p class="text">Desarrollo de aplicaciones completas, backend en PHP o Typescript.</p>
+          <p class="text">Comercio elecrónico, backoffices personalizados, aplicaciones de móviles y de escritorio.</p>
+        </div>
+      </div>
+      <div class="clearfix">
+        <div class="sm-col sm-col-6 mb3 mt3">
+          <div class="flex items-start mb1">
+            <h2 class="self-center">Formación</h2>
+            <div class="line"></div>
+          </div>
+          <p><b>Centro de estudios de informática superior Alicante.</b></p>
+          <p>Programador de gestión - Analista programador 1990 – 1992. </p>
+          <div class="flex items-start mb1 mt3">
+            <h2 class="self-center">Inglés</h2>
+            <div class="line"></div>
+          </div>
+          <p>Leer y escribir, no hablo ni entiendo fácilemte. </p>
+        </div>
+        <div class="sm-col sm-col-6 mb3">
+          <div class="flex items-start mb1 mt3">
+            <h2 class="self-center">Autodidacta</h2>
+            <div class="line"></div>
+          </div>
+          <p>Mi formación se basa en el seguimiento, testeo y aprendizage mediante artículos, cursos, libros, etc...
+            de todas las tecnologías susceptibles de mejorar la eficiencia en el desarrollo de software.</p>
+
+        </div>
+      </div>
+      <div class="clearfix">
+        <div class="sm-col sm-col-12 mb1">
+          <div class="flex items-start mb1">
+            <h2 class="self-center">Experiencia</h2>
+            <div class="line"></div>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix">
+        <div class="sm-col sm-col-4">
+          <div class="mb2">
+            <h3>Bmotion </h3>
+            <div>Desarrollo aplicaciones nocode</div>
+            <div><small>may.2022 • Actualidad • Madrid (remoto)</small></div>
+          </div>
+          <div class="mb2">
+            <h3>Indexeo Marketing</h3>
+            <div>Desarrollo web, mantenimiento</div>
+            <div><small>ene.2021 • mayo 2022 • Ibi (remoto)</small></div>
+          </div>
+          <div class="mb2">
+            <h3>Freelance</h3>
+            <div>Desarrollo web, diseño gráfico/web</div>
+            <div><small>ene.2020 • ene.2021 • Santa Pola</small></div>
+          </div>
+          <div class="mb2">
+            <h3>NovoSpain</h3>
+            <div>Desarrollo web, diseño gráfico</div>
+            <div><small>nov.2016 • ene.2020 • Elche</small></div>
+          </div>
+
+        </div>
+        <div class="sm-col sm-col-4">
+          <div class="mb2">
+            <h3>3dids.com</h3>
+            <div>Desarrollo front-end</div>
+            <div><small>mar.2016 • nov.2016 • Alicante</small></div>
+          </div>
+
+          <div class="mb2">
+            <h3>Fractal Internet Consultancy</h3>
+            <div>Desarrollo web y diseño gráfico/web</div>
+            <div><small>jun.2012 • oct.2015 • Madrid (remoto)</small></div>
+          </div>
+          <div class="mb2">
+            <h3>Baluarte Comunicación, S.L.</h3>
+            <div>Desarrollo web y diseño gráfico</div>
+            <div><small>may.2009 • nov.2012 • Santa Pola</small></div>
+          </div>
+          <div class="mb2">
+            <h3>MASA International</h3>
+            <div>Diseñador Gráfico (encargado dept.)</div>
+            <div><small>jun.2005 • sept.2008 • Torrevieja</small></div>
+          </div>
+
+        </div>
+        <div class="sm-col sm-col-4  ">
+          <div class="mb2">
+            <h3>Imprenta Hnos. Rastoll</h3>
+            <div>Diseñador Gráfico, artefinalista</div>
+            <div><small>dic.2003 • oct.2005 • Santa Pola</small></div>
+          </div>
+          <div class="mb2">
+            <h3>Cartel Rotulación, S.L.</h3>
+            <div>Diseño para exteriores, rotulación e impresión</div>
+            <div><small>feb.2000 • abr.2003 • Alicante</small></div>
+          </div>
+          <div class="mb2">
+            <h3>Rusan Copy, S.L (CopyFlash)</h3>
+            <div>Diseño para exteriores, rotulación e impresión</div>
+            <div><small>ene.1993 • ene.2000 • Alicante</small></div>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix">
+        <div class="sm-col sm-col-12 mb1 mt3">
+          <div class="flex items-start mb3">
+            <h2 class="self-center">Habilidades</h2>
+            <div class="line"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="clearfix">
+        <div class="sm-col sm-col-6  ">
+          <h3 class="self-center mb1">Front end</h3>
+            <ul class="list-reset">
+              <li>• Código mantenible y legible, convenciones de código.</li>
+              <li>• Internacionalización.</li>
+              <li>• Persistencia de datos.</li>
+              <li>• Autenticación basadas en token o sesión.</li>
+              <li>• Manejo y conversión de fechas.</li>
+              <li>• Creación programática y previsualización de PDF.</li>
+              <li>• Conexión con APIs.</li>
+              <li>• Carruseles, galerías, modal y sliders responsive.</li>
+              <li>• Tooltips, notificaciones, arrastrar y soltar ...</li>
+              <li>• Calendario interactivo.</li>
+              <li>• Validación en vivo.</li>
+              <li>• Index DB, local/session storage.</li>
+              <li>• Recepción de datos del servidor (socket.io).</li>
+              <li>• Marcado semántico.</li>
+              <li>• Web components.</li>
+              <li>• Web App.</li>
+              <li>• Cookies.</li>
+              <li>• Canvas.</li>
+              <li>• Audio y video en la web.</li>
+              <li>• Pre-procesadores.</li>
+              <li>• Desarrollo responsive, mobile first.</li>
+
+            </ul>
+        </div>
+        <div class="sm-col sm-col-6  ">
+           <ul class="list-reset">
+            <li>• Frameworks: Bootstrap, Fundation, Pure, UI-Kit, Skeleton...</li>
+            <li>• Tipografía web.</li>
+            <li>• Gráficos vectoriales (svg).</li>
+            <li>• Animaciones CSS.</li>
+            <li>• Javascript orientado a objetos.</li>
+            <li>• Javascript asíncrono.</li>
+            <li>• Solicitud/Respuestas http.</li>
+            <li>• Estructuras de datos Json.</li>
+            <li>• TypeScript.</li>
+            <li>• Aplicación modular por componentes.</li>
+            <li>• Arquitectura hexagonal.</li>
+            <li>• WebPack, flujo de trabajo webpack, vite...</li>
+            <li>• Framework Vue (preferido).</li>
+            <li>• Nuxt.</li>
+            <li>• Motores de plantillas (pug, blade, liquid.. ).</li>
+            <li>• Index DB, local/session storage.</li>
+            <li>• Gestión de estado / arquitectura flux.</li>
+            <li>• Fundamentos de UX.</li>
+            <li>• Prototipos UI.</li>
+            <li>• Flutter , Electron, App híbrida, Cordova, Ionic. App nativo.</li>
+
+          </ul>
+        </div>
+      </div>
+      <div class="clearfix">
+        <div class="sm-col sm-col-6 mt3 ">
+          <h3 class="self-center mb1 ">Back end</h3>
+          <ul class="list-reset">
+            <li>• Creación, diseño y optimización de bases de datos relacionales MySql, MariaDB...</li>
+            <li>• Migraciones y generación de datos de prueba.</li>
+            <li>• ORM Eloquent y Lucid</li>
+            <li>• Paginación y pre-proceso de información e imágenes.</li>
+            <li>• Uso de clientes http.</li>
+            <li>• Creación de APIs con autenticación.</li>
+            <li>• Gestión de sesión.</li>
+            <li>• Validación desde el servidor.</li>
+          </ul>
+        </div>
+        <div class="sm-col sm-col-6   ">
+           <ul class="list-reset">
+            <li>• Login y registro seguro.</li>
+            <li>• Gestión de ficheros desde el servidor.</li>
+            <li>• Envío de correos automatizados.</li>
+            <li>• Creación de tareas periódicas.</li>
+            <li>• Caché en disco o memoria.</li>
+            <li>• Manejo de eventos y colas</li>
+            <li>• Uso de linea de comandos Linux</li>
+            <li>• Gestión de dependencias con Composer.</li>
+            <li>• Testing http, unitarios, de navegador...</li>
+            <li>• Git, Github, Docker, IA para programar... </li>
+          </ul>
+        </div>
+      </div>
+    <div class="mb5">&nbsp;</div>
+    <div class="mb5">&nbsp;</div>
+    <div class="mb5">&nbsp;</div>
+    <div class="mb5">&nbsp;</div>
+    <div class="mb5">&nbsp;</div>
+    <div class="mb5">&nbsp;</div>
+    </div>
+
+  </div>
+
+</template>
